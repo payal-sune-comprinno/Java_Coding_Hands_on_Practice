2a655981eeb0757047155afa72d4944f0b5e5f7c
Payal Sune <payalsune123@gmail.com>
2026-01-24 23:19:48 +0530

    geeksforgeeks problem

--- Check_LL_IsCircular.java
+++ Check_LL_IsCircular.java
@@ -1,6 +1,19 @@
-class Main{
-    public static void main(String args[])
-    {
-        
+//geeksforgeeks problem:https://www.geeksforgeeks.org/problems/circular-linked-list/1
+
+
+class Solution {
+    boolean isCircular(Node head) {
+        // Your code here
+        if(head==null)
+        {
+            return false;
+            
+        }
+        Node current=head.next;
+        while(current!=null && current!=head)
+        {
+            current=current.next;
+        }
+        return current==head;
     }
 }
